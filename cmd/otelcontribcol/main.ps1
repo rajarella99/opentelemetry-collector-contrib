ab0a4afc899d9ec40df9843d14f84bbf100a75c9
root <root@MLBBTL-109240.in.marlabs.com>
2022-03-04 16:17:38 +0530

    windows powershell script and mainpublicprivate.pfx codesigning cert added

---
+++ cmd/otelcontribcol/main.ps1
@@ -0,0 +1,11 @@
+﻿$VerbosePreference = 'continue'
+go build -x
+$FileName = "D:\build\opentelemetry-collector-contrib\cmd\otelcontribcol\otelcontribcol.exe"
+while (!(Test-Path $FileName)) { Start-Sleep 10 }
+if (Test-Path $FileName) {
+  signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f .\mainpublicprivate.pfx /p apple@123 $FileName
+}
+
+Write-Host -ForegroundColor Green "The OpenTelemetry Executable is successfully Signed"
+#signtool.exe sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f .\mainpublicprivate.pfx /p apple@123 .\hello.exe
+
